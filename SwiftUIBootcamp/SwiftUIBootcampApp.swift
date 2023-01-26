4e5a881dc4bc50baf136e1e6b2eaabd5d91b8133
nikolascoelho <nikolasgianoglou.coelho-ext@ab-inbev.com>
2023-01-26 12:07:51 -0300

    Creation of the initial project

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Nikolas Gianoglou Coelho on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
